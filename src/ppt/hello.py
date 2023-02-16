8591c40942d346d3fc28ea473a2453e12684aafa
Michael-Cohen <michael.cohen21@gmail.com>
2023-02-16 13:51:12 +0100

    fix: added wow

--- src/ppt/hello.py
+++ src/ppt/hello.py
@@ -1,5 +1,11 @@
 def say_hello_with_message(msg):
     print(f"hello, the message is: {msg}")
 
+print("voilà")
+print("je suis à la maison")
+
+def wow(msg):
+    say_hello_with_message(f"wow: {msg}")
+
 if __name__ == '__main__':
     say_hello_with_message(1)
