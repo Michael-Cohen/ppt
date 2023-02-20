757eeb6c89b99edbb7973eaaf00cdc0a44e1e463
Michael-Cohen <michael.cohen21@gmail.com>
2023-02-20 14:59:02 +0100

    refactor: Other random changes

--- src/pptm/salut.py
+++ src/pptm/salut.py
@@ -1,16 +1,16 @@
 import numpy as np
 
 
-def say_hello_with_message(m: str) -> str:
+def say_hello_with_message(message: str) -> str:
     """Awesome function to say hello with message.
 
     Args:
-        m (str): The message of your dreams.
+        message (str): The message of your dreams.
 
     Returns:
         str: The standard sentence with your message.
     """
-    return f"hello, the message is: {m}"
+    return f"hello, the message is: {message}"
 
 
 def wow(quantity: float) -> str:
