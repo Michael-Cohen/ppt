f7ada03b42bbf03b7ae88f9f8e6c3ed3a151e591
Michael-Cohen <michael.cohen21@gmail.com>
2023-02-24 11:47:28 +0100

    refactor: Experiment a little bit

--- unpackaged/replace.py
+++ unpackaged/replace.py
@@ -2,6 +2,20 @@ from jinja2 import Environment, FileSystemLoader
 
 
 def set_query(query_path: str, replacement: dict) -> str:
+    """Dynamically creates an SQL query from a template.
+
+    Note: Jinja2 has another feature called "template inheritance"
+    for replacing whole blocks of the template. This is not used
+    in this function.
+
+    Args:
+        query_path (str): Path to the SQL query template.
+        replacement (dict): Dictionary {variable -> replacement value} to
+            be applied.
+
+    Returns:
+        str: The query with replacements performed.
+    """
     # Create a Jinja environment
     env = Environment(loader=FileSystemLoader("src/pptm"))
     # Load the template
